0bd181d2a007133b547af4c25e6d51fa2cdc083f
Watcharapon nuisin <38254231+godsnew2542@users.noreply.github.com>
2021-05-04 11:11:06 +0700

    bug

--- javascript/User.js
+++ javascript/User.js
@@ -12,11 +12,6 @@ function test() {
   let Tphone = document.getElementById("Tphone").value;
   let status = document.querySelector('input[name="status"]:checked').value;
 
-  let getUsertables = `${API}/api/usertables`;
-  $.get(getUsertables, function (Usertables) {
-    console.log(Usertables);
-  });
-
   if (
     fname == "" ||
     lname == "" ||
@@ -40,7 +35,47 @@ function test() {
 function checkName() {
   let fname = document.getElementById("Fname").value;
   let lname = document.getElementById("Lname").value;
+  let check_name = false;
 
+  if (fname == "" || lname == "") {
+    console.log("ใส่ข้อมูลก่อนตะ");
+  } else {
+    let getUsertables = `${API}/api/usertables`;
+    $.get(getUsertables, function (Usertables) {
+      for (let index = 0; index < Usertables.length; index++) {
+        const element = Usertables[index];
+        const uFname = element.uFname;
+        const uLname = element.uLname;
+        const uPosition = element.uPosition;
+        // const uLname = element.uLname;
+        // console.log(element);
+        if (uFname == fname && uLname == lname) {
+          addDataByUser1(element);
+          check_name = true;
+        }
+      }
+      if (check_name == false) {
+        console.log("ไม่มีข้อมูลที่ตรงกัน");
+      }
+    });
+  }
+}
+
+function addDataByUser1(ele) {
+  console.log(ele);
+  let select = document.all;
+
+  document.getElementById("position").value = ele.uPosition;
+  if (select.TypeP.value == "0") {
+    select.TypeP.value = ele.uType;
+  }
+
+  document.getElementById("positionJ").value = ele.uAffiliation;
+  document.getElementById("idSalary").value = ele.uSalaryid;
+  document.getElementById("phone").value = ele.uPhone;
+  document.getElementById("Tphone").value = ele.uTel;
+
+  // document.querySelector('input[name="status"]:checked').value = uStatus;
 }
 
 function UserPage2() {
@@ -54,13 +89,12 @@ function UserPage2() {
     fname == "" ||
     lname == "" ||
     phone == "" ||
-    Tphone == ""||
+    Tphone == "" ||
     salary == ""
   ) {
     alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
   }
 }
-
 
 function UserPage3() {
   let fname = document.getElementById("Fname").value;
@@ -68,7 +102,7 @@ function UserPage3() {
   let position = document.getElementById("position").value;
   let TypeP = $("#TypeP option:selected").val();
   let positionJ = document.getElementById("positionJ").value;
-  let idPo= document.getElementById("idPo").value;
+  let idPo = document.getElementById("idPo").value;
   let phone = document.getElementById("phone").value;
   let Tphone = document.getElementById("Tphone").value;
   let salary = document.getElementById("salary").value;
@@ -81,16 +115,15 @@ function UserPage3() {
     positionJ == "" ||
     idPo == "" ||
     phone == "" ||
-    Tphone == ""||
+    Tphone == "" ||
     salary == ""
   ) {
     alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
-   }
-   else {
+  } else {
     if (status == "สมรส") {
-     location.href="./User-4.html";
+      location.href = "./User-4.html";
     } else {
-      location.href="./User-5.html";
+      location.href = "./User-5.html";
     }
   }
 }
@@ -106,7 +139,7 @@ function UserPage4() {
     fname == "" ||
     lname == "" ||
     phone == "" ||
-    Tphone == ""||
+    Tphone == "" ||
     salary == ""
   ) {
     alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
@@ -114,33 +147,23 @@ function UserPage4() {
 }
 
 function UserPage5() {
- 
   let total = document.getElementById("total").value;
   let Psalary = document.getElementById("Psalary").value;
   let salary = document.getElementById("salary").value;
 
-  if (
-    total == "" ||
-    Psalary == ""||
-    salary == ""
-  ) {
+  if (total == "" || Psalary == "" || salary == "") {
     alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
-  }else{
-    location.href="./UserConfirm.html";
+  } else {
+    location.href = "./UserConfirm.html";
   }
 }
 
 function UserConfirm() {
   let check = document.getElementById("myChecked").checked;
 
-
-  if (
-    total == "" ||
-    Psalary == ""||
-    salary == ""
-  ) {
+  if (total == "" || Psalary == "" || salary == "") {
     alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
-  }else{
-    location.href="./UserConfirm.html";
+  } else {
+    location.href = "./UserConfirm.html";
   }
 }
