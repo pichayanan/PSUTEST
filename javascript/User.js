3f751beb499d1b797000760e3a7ba49f771951e6
pichayanan <palm.1611@gmail.com>
2021-05-04 09:09:05 +0700

    user-3

--- javascript/User.js
+++ javascript/User.js
@@ -23,7 +23,7 @@ function test() {
     phone == "" ||
     Tphone == ""
   ) {
-    alert("ใส่ข้อมูลให้ถูกหิดที");
+    alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
   } else {
     if (status == "โสด" || status == "หม้าย") {
         location.href = '../User-3.html';
@@ -32,4 +32,62 @@ function test() {
 
     }
   }
+}
+
+function checkName() {
+  let fname = document.getElementById("Fname").value;
+  let lname = document.getElementById("Lname").value;
+
+}
+
+function test2() {
+  let fname = document.getElementById("Fname").value;
+  let lname = document.getElementById("Lname").value;
+  let phone = document.getElementById("phone").value;
+  let Tphone = document.getElementById("Tphone").value;
+  let salary = document.getElementById("salary").value;
+
+  if (
+    fname == "" ||
+    lname == "" ||
+    phone == "" ||
+    Tphone == ""||
+    salary == ""
+  ) {
+    alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
+  }
+}
+
+
+function test3() {
+  let fname = document.getElementById("Fname").value;
+  let lname = document.getElementById("Lname").value;
+  let position = document.getElementById("position").value;
+  let TypeP = $("#TypeP option:selected").val();
+  let positionJ = document.getElementById("positionJ").value;
+  let idPo= document.getElementById("idPo").value;
+  let phone = document.getElementById("phone").value;
+  let Tphone = document.getElementById("Tphone").value;
+  let salary = document.getElementById("salary").value;
+  let status = document.querySelector('input[name="status"]:checked').value;
+  if (
+    fname == "" ||
+    lname == "" ||
+    position == "" ||
+    TypeP == "0" ||
+    positionJ == "" ||
+    idPo == "" ||
+    phone == "" ||
+    Tphone == ""||
+    salary == ""
+  ) {
+    alert("กรุณาระบุข้อมูลให้ครบถ้วนและถูกต้อง");
+   }
+   // else {
+  //   if (status == "สมรส") {
+  //    location.href="./User-4.html";
+  //   } else {
+  //     location.href="./User-5.html";
+  //   }
+  // }
 }
